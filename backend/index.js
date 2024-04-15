64ec2a1d3ba385a49022b29af0bd17c34708b8b9
Rimjhim Mukherjee <134839601+RM-CS@users.noreply.github.com>
2024-04-15 13:16:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,23 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
-const port = 5000
+app.use(cors(
+  {
+    origin: ["https://food-express-delivery-api.vercel.app"],
+    method: ["POST", "GET"],
+    credentials: true
+  }
+));
+
 const mongodb = require("./db")
 mongodb()
 
-
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
 
 app.use(express.json())
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  res.send('Hello!')
 })
